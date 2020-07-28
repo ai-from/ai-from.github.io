f15782a0de656b9d8c6915b7f294db3babea44a9
Aifrom <codemusic2020@gmail.com>
2020-07-28 21:36:35 +0300

    carpet-shop

---
+++ carpet-shop/precache-manifest.b2f451b8df9343d584802eb34a8e0f2a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42111f6c6fa966e8315",
+    "url": "/css/app.510947ab.css"
+  },
+  {
+    "revision": "3a4dd42e9aee969e6f69",
+    "url": "/css/chunk-21bbe4ac.6aa56c2a.css"
+  },
+  {
+    "revision": "0b78c80341554fe5024a",
+    "url": "/css/chunk-92312eb8.22a53ddd.css"
+  },
+  {
+    "revision": "abedecc12af1d44d1e07",
+    "url": "/css/chunk-b13702cc.aabd0b01.css"
+  },
+  {
+    "revision": "a5b9ddf6b6ef27151b59",
+    "url": "/css/chunk-f1c955b6.0ebf5857.css"
+  },
+  {
+    "revision": "542ae928d4380f0af38f",
+    "url": "/css/chunk-vendors.d64f6a5d.css"
+  },
+  {
+    "revision": "2b575ee20cba7cdd7fab15123f08edbb",
+    "url": "/img/avatar.2b575ee2.jpg"
+  },
+  {
+    "revision": "fcd5fb7160db87a608bed8e5495c68ec",
+    "url": "/img/logo.fcd5fb71.png"
+  },
+  {
+    "revision": "76b65106e467a91c53b38272d420f6c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b42111f6c6fa966e8315",
+    "url": "/js/app.d0d4a769.js"
+  },
+  {
+    "revision": "3a4dd42e9aee969e6f69",
+    "url": "/js/chunk-21bbe4ac.20ff9a7c.js"
+  },
+  {
+    "revision": "ed5e5156049805b349ba",
+    "url": "/js/chunk-2d0e5e97.06d63873.js"
+  },
+  {
+    "revision": "0b78c80341554fe5024a",
+    "url": "/js/chunk-92312eb8.a8c031fd.js"
+  },
+  {
+    "revision": "abedecc12af1d44d1e07",
+    "url": "/js/chunk-b13702cc.76d64e1c.js"
+  },
+  {
+    "revision": "a5b9ddf6b6ef27151b59",
+    "url": "/js/chunk-f1c955b6.f43ccd6b.js"
+  },
+  {
+    "revision": "542ae928d4380f0af38f",
+    "url": "/js/chunk-vendors.bd035394.js"
+  },
+  {
+    "revision": "125582aae963cc289c1b464cb6797472",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
