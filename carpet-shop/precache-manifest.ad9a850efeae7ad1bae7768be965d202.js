c5af8f4d7ae5b6013ea7ebde42749579f6d20921
Aifrom <codemusic2020@gmail.com>
2020-07-28 23:08:16 +0300

    carpet-shop

---
+++ carpet-shop/precache-manifest.ad9a850efeae7ad1bae7768be965d202.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3156d453ac7b83b701",
+    "url": "/carpet-shop/css/app.510947ab.css"
+  },
+  {
+    "revision": "3a4dd42e9aee969e6f69",
+    "url": "/carpet-shop/css/chunk-21bbe4ac.6aa56c2a.css"
+  },
+  {
+    "revision": "0b78c80341554fe5024a",
+    "url": "/carpet-shop/css/chunk-92312eb8.22a53ddd.css"
+  },
+  {
+    "revision": "abedecc12af1d44d1e07",
+    "url": "/carpet-shop/css/chunk-b13702cc.aabd0b01.css"
+  },
+  {
+    "revision": "a5b9ddf6b6ef27151b59",
+    "url": "/carpet-shop/css/chunk-f1c955b6.0ebf5857.css"
+  },
+  {
+    "revision": "a82d5f859cb9dcb193fb",
+    "url": "/carpet-shop/css/chunk-vendors.d64f6a5d.css"
+  },
+  {
+    "revision": "2b575ee20cba7cdd7fab15123f08edbb",
+    "url": "/carpet-shop/img/avatar.2b575ee2.jpg"
+  },
+  {
+    "revision": "fcd5fb7160db87a608bed8e5495c68ec",
+    "url": "/carpet-shop/img/logo.fcd5fb71.png"
+  },
+  {
+    "revision": "9cbdad42dc0c4163281c8684f9f77390",
+    "url": "/carpet-shop/index.html"
+  },
+  {
+    "revision": "cf3156d453ac7b83b701",
+    "url": "/carpet-shop/js/app.2297c782.js"
+  },
+  {
+    "revision": "3a4dd42e9aee969e6f69",
+    "url": "/carpet-shop/js/chunk-21bbe4ac.20ff9a7c.js"
+  },
+  {
+    "revision": "ed5e5156049805b349ba",
+    "url": "/carpet-shop/js/chunk-2d0e5e97.06d63873.js"
+  },
+  {
+    "revision": "0b78c80341554fe5024a",
+    "url": "/carpet-shop/js/chunk-92312eb8.a8c031fd.js"
+  },
+  {
+    "revision": "abedecc12af1d44d1e07",
+    "url": "/carpet-shop/js/chunk-b13702cc.76d64e1c.js"
+  },
+  {
+    "revision": "a5b9ddf6b6ef27151b59",
+    "url": "/carpet-shop/js/chunk-f1c955b6.f43ccd6b.js"
+  },
+  {
+    "revision": "a82d5f859cb9dcb193fb",
+    "url": "/carpet-shop/js/chunk-vendors.8934c632.js"
+  },
+  {
+    "revision": "125582aae963cc289c1b464cb6797472",
+    "url": "/carpet-shop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/carpet-shop/robots.txt"
+  }
+]);
