d96336bde10f0c2999be76dcfec16aaee5823043
Aifrom <codemusic2020@gmail.com>
2020-08-10 15:39:40 +0300

    add todo

---
+++ todo/precache-manifest.f3f22e87758c9ec13c1afe0a3d3cb48a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d66c22034227901d32",
+    "url": "/todo/css/app.83cdacb8.css"
+  },
+  {
+    "revision": "ba4cfd1a59ba482be445",
+    "url": "/todo/css/chunk-3c9147eb.99919a56.css"
+  },
+  {
+    "revision": "454c96dc5cbdfc23f31b",
+    "url": "/todo/css/chunk-515d8548.aeac38d3.css"
+  },
+  {
+    "revision": "0d3bc93843cc1cd87934",
+    "url": "/todo/css/chunk-vendors.d64f6a5d.css"
+  },
+  {
+    "revision": "ebf5ee9925e9c10dbe2964a9bde7861d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "16d66c22034227901d32",
+    "url": "/todo/js/app.7201e9e8.js"
+  },
+  {
+    "revision": "664fe032f0b525b12f69",
+    "url": "/todo/js/chunk-2d22996d.fad025d1.js"
+  },
+  {
+    "revision": "ba4cfd1a59ba482be445",
+    "url": "/todo/js/chunk-3c9147eb.f0f3a91a.js"
+  },
+  {
+    "revision": "454c96dc5cbdfc23f31b",
+    "url": "/todo/js/chunk-515d8548.e96e2b10.js"
+  },
+  {
+    "revision": "0d3bc93843cc1cd87934",
+    "url": "/todo/js/chunk-vendors.ac4e3d9a.js"
+  },
+  {
+    "revision": "a3f9c1af7f5541a09b80fefe52ab5505",
+    "url": "/todo/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/todo/robots.txt"
+  }
+]);
